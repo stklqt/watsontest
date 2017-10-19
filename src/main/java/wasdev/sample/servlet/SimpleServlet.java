e0ed5d34579827228183062cbafdbcd80554c922
skleeberg <skleeberg@ite00407-mac.fritz.box>
2017-10-20 01:18:39 +0200

    first commit

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -1,17 +1,21 @@
 package wasdev.sample.servlet;
 
-import java.io.IOException;
+import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
+import com.ibm.watson.developer_cloud.language_translator.v2.util.Language;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet("/SimpleServlet")
+@WebServlet(“/SimpleServlet”)
 public class SimpleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -21,7 +25,15 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        response.getWriter().print(translateToSpanish("This is a test"));
+    }
+
+    private String translateToSpanish(String text){
+
+        LanguageTranslator service = new LanguageTranslator();
+        service.setUsernameAndPassword("uname", "pwd"); //FIXME anpassen
+        TranslationResult translationResult = service.translate(text, Language.ENGLISH, Language.GERMAN).execute();
+        return translationResult.getTranslations().get(0).toString();
     }
 
 }
