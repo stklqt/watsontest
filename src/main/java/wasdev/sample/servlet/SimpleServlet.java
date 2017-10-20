d86ef9f3c5079c92dd5ff5472a2d849f8ec71073
steffen <stkl@quantentunnel.de>
2017-10-20 17:53:40 +0200

    add other testdata

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -27,7 +27,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print(translateToGerman("This is a test"));
+        response.getWriter().print(translateToGerman("This is a really great test"));
     }
 
     private String translateToGerman(String text) {
